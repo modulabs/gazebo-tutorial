cbc67d0659e9c356526070332e3e75119cd661b2
elton-choi <babchol@gmail.com>
2018-01-31 07:07:52 +0900

    Add new tutorials and edit folder name.

---
+++ build_a_robot/animatedbox/build/CMakeFiles/independent_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/independent_listener.dir/independent_listener.cc.o"
+  "independent_listener.pdb"
+  "independent_listener"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/independent_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_a_robot/animatedbox/build/CMakeFiles/integrated_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/integrated_main.dir/integrated_main.cc.o"
+  "integrated_main.pdb"
+  "integrated_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/integrated_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
